a24719ae8d1a52aabdcebe3c042e5f6948ca14d3
ankit7hub <155054521+ankit7hub@users.noreply.github.com>
2023-12-31 00:47:32 +0530

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,14 @@
+function webcam(){
+document.getElementById("videoElement").style.display="block";
+
+var video = document.querySelector("#videoElement");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
+}};
